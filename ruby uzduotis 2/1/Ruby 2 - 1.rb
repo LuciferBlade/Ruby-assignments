11dc695935cedb570c41bd4da8b2db5ae3a9cc43
Mindaugas Burvys <38848998+LuciferBlade@users.noreply.github.com>
2019-09-25 05:04:30 +0300

    Įkelta užduotis 1
    Įkelta užduotis 1, bet be 150000 įrašų failo. Šis failas užima per daug atminties jam patalpinti. Jo vietoje yra test.txt failas, kuriame yra maža dalis minėto failo įrašų ir juo buvo testuojama programa, kad pagreitinti jos vykdymo laiką.

---
+++ ruby uzduotis 2/1/Ruby 2 - 1.rb
@@ -0,0 +1,31 @@
+input_file = "/home/blade/ruby stuf/ruby uzduotis 2/1/test.txt"#1500000 Sales Records.csv"
+output_file = "/home/blade/ruby stuf/ruby uzduotis 2/1/output.txt"
+
+file1 = File.new(input_file, "r")
+file2 = File.new(output_file, "w")
+
+file1.each do |line|
+	parts = line.split(',')
+	parts.each do |i|
+		if i.match(/^[-]?\d+?$/)
+			file2.print "Integer"
+		elsif i.match(/^[-]?(\d+)+[.]\d+?$/)
+			file2.print "Float"
+		elsif i.size == 1
+			file2.print "Char"
+		elsif i.match(/^(0?[1-9]|1[0-2])?[\/](0?[1-9]|[12]\d|3[01])?[\/]\d{4}$/)
+			file2.print "Date"
+		elsif
+			file2.print "String"
+		end
+		if i != parts.last
+			file2.print(",")
+		end
+	end
+	file2.puts("")
+end
+
+puts "done"
+
+file2.close
+file1.close
